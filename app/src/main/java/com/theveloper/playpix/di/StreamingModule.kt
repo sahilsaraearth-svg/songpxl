cca402deb1d37037f440145f13256bc2ac54c327
sahilsaraearth-svg <sahilsaraearth@users.noreply.github.com>
2026-05-18 12:50:23 +0000

    fix: revert base URL to jiosavan-api2.vercel.app (saavn.dev is dead)

--- app/src/main/java/com/theveloper/playpix/di/StreamingModule.kt
+++ app/src/main/java/com/theveloper/playpix/di/StreamingModule.kt
@@ -34,7 +34,7 @@ object StreamingModule {
     @JioSaavnRetrofit
     fun provideJioSaavnRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://saavn.dev/")
+            .baseUrl("https://jiosavan-api2.vercel.app/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
